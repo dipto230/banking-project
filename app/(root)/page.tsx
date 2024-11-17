0f06ec3e692107c2d3cbf074e3ca4f61708fefc1
dipto230 <diptoroy230305@gmail.com>
2024-11-17 20:13:13 +0600

    right section half done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,39 +1,36 @@
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
-import HeaderBox from '@/components/ui/HeaderBox'
-import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox';
+import React from 'react';
 
 const Home = () => {
-    const loggedIn ={ firstName : 'Dipto'};
+  const loggedIn = { firstName: 'Dipto', email: 'contact@gmail.com' };
+
   return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-                 type="greeting"
-                 title ="welcome"
-                 user ={loggedIn ?.firstName||'Guest'}
-                 subtext = "Access and manage your account and  transaction  efficiently"
-                
-                
-                />
-                <TotalBalanceBox 
-                accounts={[]}
-                totalBanks ={1}
-                totalCurrentBalance ={1230.56}
-                
-                
-                
-                />
-                    
-                
-                   
-
-            </header>
-
-        </div>
-
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1230.56}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transaction={[]}
+        banks={[{},{}]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
